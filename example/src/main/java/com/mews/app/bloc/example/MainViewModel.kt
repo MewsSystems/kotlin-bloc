00904483ca8f428178e0585201df6765a5eae89a
ookami-kb <ookami.kb@gmail.com>
2020-07-08 15:24:46 +0200

    Fix example

--- example/src/main/java/com/mews/app/bloc/example/MainViewModel.kt
+++ example/src/main/java/com/mews/app/bloc/example/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.mews.app.bloc.example
 
-import com.mews.app.bloc.Sink
 import com.mews.app.bloc.android.BlocViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.transform
@@ -8,9 +7,9 @@ import kotlinx.coroutines.flow.transform
 class MainViewModel : BlocViewModel<MainEvent, MainState>() {
     override val initialState: MainState = MainState()
 
-    override suspend fun Sink<MainState>.mapEventToState(event: MainEvent) = when (event) {
-        MainEvent.Incremented -> add(state.copy(value = state.value + 1))
-        MainEvent.Decremented -> add(state.copy(value = state.value - 1))
+    override suspend fun mapEventToState(event: MainEvent, emitState: suspend (MainState) -> Unit) = when (event) {
+        MainEvent.Incremented -> emitState(state.copy(value = state.value + 1))
+        MainEvent.Decremented -> emitState(state.copy(value = state.value - 1))
     }
 
     override fun transformEvents(events: Flow<MainEvent>): Flow<MainEvent> = events.transform {
